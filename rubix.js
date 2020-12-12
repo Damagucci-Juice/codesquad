cda4f106604d4e5dc2d97706cc7559a45d129b97
Damagucci-Juice <jsr9874@gmail.com>
2020-12-13 06:19:56 +0900

    Add switch-case

--- rubix.js
+++ rubix.js
@@ -13,7 +13,31 @@ var time = document.getElementById('time');
 
 var cube = {
     'faces': [left, bottom, front, back, top, right],
+    'colors': ['W', 'B', 'G', 'Y', 'O', 'R'],
+    'orders': [],
     'current': 0
 };
 
-cube.input() = function() {};
+cube.input = function() {
+    const inputString = input.value;
+    this.orders = inputString.toUpperCase().split('');
+};
+cube.input();
+
+cube.switch = function () {
+    for (let i = 0; i < this.orders.length; i++) {
+        switch (this.orders[i]) {
+            case '\'':
+                this.orders[i-1] += '\'';
+                let x = this.orders.splice(i,1);
+                i -= 1;
+                break;
+            case '2':
+                this.orders[i] = this.orders[i-1];
+                break;
+            default:
+                console.log(this.orders[i]);
+        }
+    }
+};
+cube.switch();
