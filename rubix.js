d8bf416e8aed937a0a46e5261301dbf91b165c7c
Damagucci-Juice <jsr9874@gmail.com>
2020-12-13 05:36:00 +0900

    step3: construct rubik's cube layout.

---
+++ rubix.js
@@ -0,0 +1,19 @@
+var face = document.getElementsByTagName('td');
+var input = document.getElementById('order');
+var check = document.getElementById('check');
+var count = document.getElementById('count');
+var time = document.getElementById('time');
+
+
+
+
+
+
+
+
+var cube = {
+    'faces': [left, bottom, front, back, top, right],
+    'current': 0
+};
+
+cube.input() = function() {};
